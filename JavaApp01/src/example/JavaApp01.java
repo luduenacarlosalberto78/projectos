64ee5a14696888eed20b72f1cdf9b22bd2d7fe50
martindapol <martindapol@gmail.com>
2020-04-17 14:08:38 -0300

    Proyecto contrasenias

--- JavaApp01/src/example/JavaApp01.java
+++ JavaApp01/src/example/JavaApp01.java
@@ -1,5 +1,8 @@
 package example;
 
+import java.util.Calendar;
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -16,7 +19,27 @@ public class JavaApp01 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Primer problema: calcular la edad y determinar si es mayor de edad
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Ingrese su año de nacimiento");
+        int anio = input.nextInt();
+        int actual = Calendar.getInstance().get(Calendar.YEAR);
+
+        //calculamos la edad:
+        int edad = actual - anio;
+
+        //mostrar resultado:
+        System.out.println("Su edad a la fecha es: " + edad);
+        if (edad >= 18) {
+            System.out.println("Usted es mayor de edad!");
+        } else if (edad < 1) {
+            System.out.println("Es un bebe!!!");
+
+        } else {
+            System.out.println("Es menor de edad");
+        }
     }
     
 }
